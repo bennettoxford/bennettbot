8dcdf69ec88224c17c1a113bfcf15dfba6e465d1
Alice Wong <alice.wong@thedatalab.org>
2025-05-12 11:24:57 +0100

    Create utils module for `argparse.Action` classes
    We will need to add another string splitting action,
    so create a utils module to store argparse actions.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+import argparse
+
+from workspace.utils.argparse import SplitString
 from workspace.utils.blocks import truncate_text
 
 
@@ -6,3 +9,14 @@ def test_truncate_text():
     truncated_text = truncate_text(original_text)
     assert len(truncated_text) == 3000
     assert truncated_text.endswith("...")
+
+
+def test_split_string():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--values",
+        action=SplitString,
+        help="A space-separated quoted string",
+    )
+    args = parser.parse_args(["--values", "value1 value2 value3"])
+    assert args.values == ["value1", "value2", "value3"]
